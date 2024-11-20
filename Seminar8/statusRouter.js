da6ed075308c1692576d1cb32d2bd276e383562b
Siam888 <tudorianculescu@yahoo.com>
2024-11-20 23:46:02 +0200

    add error middleware

--- Seminar8/statusRouter.js
+++ Seminar8/statusRouter.js
@@ -1,15 +1,23 @@
-
 const express = require('express');
 
 const statusRouter = express.Router();
 
+/**
+ * Ex.4 Endpoint pentru verificarea statusului serverului.
+ * Această rută întoarce un răspuns JSON cu un mesaj de confirmare și statusul 200,
+ * indicând că serverul funcționează corect.
+ */
 statusRouter.get('/status', (req, res) => {
+    res.status(200).json({ message: 'Server is alive and well' });
+});
 
-    //6. Error middleware
-    throw new Error("Error")
+/**
+ * Rută separată pentru testarea middleware-ului de gestionare a erorilor.
+ * Declanșează intenționat o eroare pentru a verifica comportamentul handler-ului de erori.
+ */
+statusRouter.get('/test-error', (req, res, next) => {
+    // Creează o eroare simulată și o transmite către middleware-ul de erori
+    throw new Error('This is a test error!');
+});
 
-    res.status(200).json({ messsage: 'Server is alive and well' })
-})
-
-
-module.exports = statusRouter
+module.exports = statusRouter;
